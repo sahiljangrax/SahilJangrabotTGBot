37fa8c40d49c25d64cf4b15341c560c3e89728ff
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2024-01-23 14:54:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sahilysense/TG_BOTZ /TG_BOTZ 
+  git clone https://github.com/sahiljangrax/TG_BOTzz /TG_BOTZzz 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TG_BOTZ
+  git clone $UPSTREAM_REPO /TG_BOTZzz
 fi
-cd /TG_BOTZ 
+cd /TG_BOTZzz 
 pip3 install -U -r requirements.txt
 echo "Starting TG_BOTZ 😎...."
 python3 bot.py    
