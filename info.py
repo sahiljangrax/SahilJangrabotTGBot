6003536a39cf5ef980b4c4f2a6df210987b7ced4
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2024-09-28 23:16:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,8 +96,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://chemical-norine-sahiljangrabot-0bd73b68.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://chemical-norine-sahiljangrabot-0bd73b68.koyeb.app/".format(FQDN, PORT)
+URL = "https://current-maybelle-sahiljangrabot-8da78e82.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://current-maybelle-sahiljangrabot-8da78e82.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://chemical-norine-sahiljangrabot-0bd73b68.koyeb.app/".format(FQDN)
+    URL = "https://current-maybelle-sahiljangrabot-8da78e82.koyeb.app/".format(FQDN)
 else:
-    URL = "https://chemical-norine-sahiljangrabot-0bd73b68.koyeb.app/".format(FQDN)
+    URL = "https://current-maybelle-sahiljangrabot-8da78e82.koyeb.app/".format(FQDN)
 
 
 
